3a1cfe3c1d0ce661aacc5cf95e419376e0cac7b7
ashrhmn25 <139636237+ashrhmn25@users.noreply.github.com>
2026-01-24 00:56:29 +0530

    Updated python to allow access to external address.

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world! deployed using AWS Code Build and Deploy with the help of AWS Pipeline'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
 
 # To final check of aws code build and code deploy are working or not.
